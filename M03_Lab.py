1aafab03f5ac28ce49eb47ac60541b8d9acd1a37
Joe Douglas <90574985+jdoug8@users.noreply.github.com>
2022-11-28 00:17:05 -0600

    Update M03_Lab.py

--- M03_Lab.py
+++ M03_Lab.py
@@ -1,12 +1,27 @@
+"""
+
+Vehicle_Questionnaire.py
+
+Author: Joe Douglas
+
+This program is designed to take in the user's inputs through a series of questions and then have them
+go through classes to evaluate the data, and then output the data for the user to read.
+
+
+"""
+
 
 class Vehicle:
+    """Parent class designed to take the user's input for vehicle"""
+
     def __init__(self, type):
         self.type = type
 
 
-
 class Automobile(Vehicle):
-    def __init__(self, year, make, model, doors, roof):
+    """Class designed to take the user's input for the description of the vehicle"""
+
+    def __init__(self, type, year, make, model, doors, roof):
         super().__init__(type)
         self.year = year
         self.make = make
@@ -14,15 +29,26 @@ class Automobile(Vehicle):
         self.doors = doors
         self.roof = roof
 
-user_year = Automobile(input("Please enter the year of your vehicle: "))
-user_make = Automobile(input("Please enter the maker of your vehicle: "))
-user_model = Automobile(input("Please enter the model of your vehicle: "))
-user_doors = Automobile(input("Please enter the number of doors on your vehicle: "))
-user_roof = Automobile(input("Please enter type of roof on your vehicle: "))
 
-print("Vehicle type:", user_type.type)
-print("Year:", user_year.model)
-print("Make:", user_make.make)
-print("Model:", user.model.model)
-print("Number of doors:", user_doors.doors)
-print("Type of roof:", user_roof.roof)
+user_type = (input("Please enter the type of your vehicle: "))
+user_year = (input("Please enter the year of your vehicle: "))
+user_make = (input("Please enter the maker of your vehicle: "))
+user_model = (input("Please enter the model of your vehicle: "))
+user_doors = (input("Please enter the number of doors on your vehicle: "))
+user_roof = (input("Please enter type of roof on your vehicle: "))
+
+# A series of inputs designed to store the information on the vehicle before going through classes
+
+vehicle_1 = Automobile(user_type, user_year, user_make, user_model, user_doors, user_roof)
+
+
+print("\n Vehicle Specifications \n")
+
+print("Vehicle type:", vehicle_1.type)
+print("Year:", vehicle_1.model)
+print("Make:", vehicle_1.make)
+print("Model:", vehicle_1.model)
+print("Number of doors:", vehicle_1.doors)
+print("Type of roof:", vehicle_1.roof)
+
+# Printing results
